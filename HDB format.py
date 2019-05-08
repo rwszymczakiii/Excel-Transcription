6d235f10da9ded7b59462a8e280e78a846961e46
rwszymczakiii <rwszymczakiii@gmail.com>
2019-05-08 15:08:33 -0400

    Update HDB format.py

--- HDB format.py
+++ HDB format.py
@@ -5,28 +5,41 @@ Created on Wed Apr  3 10:10:38 2019
 @author: rszymczak
 """
 
+
+# =============================================================================
+# imports
+# =============================================================================
 import os
+import pandas as pd
+import pandas.io.formats.excel
+#import datetime
+
+# =============================================================================
+# to extract the data from a file
+# =============================================================================
 os.chdir('c:/users/rszymczak/desktop')
 
-import pandas as pd
+chsc_read = pd.read_excel('HDB2.xlsx', 'CHO-K1,HC,SUMBERGED,CINT')
 
+## =============================================================================
+## formating the new sheet
+## =============================================================================
+pandas.io.formats.excel.header_style = None
 
-chsc = pd.read_excel('HDB.xlsx', 'CHO-K1,HC,SUMBERGED,CINT', header=None)
-writer = pd.ExcelWriter('HDB format test.xlsx', engine='xlsxwriter') 
-chsc.to_excel(writer, 'CHO-K1,HC,SUMBERGED,CINT', header=None, index=None)
+chsc_writer = pd.ExcelWriter('HDB format test.xlsx', engine='xlsxwriter') 
 
+chsc_read.to_excel(chsc_writer, 'CHO-K1,HC,SUMBERGED,CINT', header=None)
 
-HDB = writer.book
-chsc = writer.sheets['CHO-K1,HC,SUMBERGED,CINT']
-
-                             
-ID_format = HDB.add_format(
+HDB_workbook = chsc_writer.book
+chsc_worksheet = chsc_writer.sheets['CHO-K1,HC,SUMBERGED,CINT']
+                        
+ID_format = HDB_workbook.add_format(
         {'align': 'center', 
          'valign': 'vcenter',
          'text_wrap':True}
         )
 
-data_format = HDB.add_format(
+data_format = HDB_workbook.add_format(
     {'num_format':'#,##0.00',
      'align': 'center',
      'valign': 'vcenter',
@@ -34,31 +47,55 @@ data_format = HDB.add_format(
     )
 
 
-chsc.set_zoom(50)
 
-chsc.set_column('A:C', 15, ID_format)
-chsc.set_column('D:AA', 12, data_format)
-chsc.set_column('AB:AG', 12, ID_format)
+chsc_worksheet.set_zoom(50)
 
-#chsc.merge_range('A3:C3', '')
-#chsc.merge_range('A4:C4', '')
-#chsc.merge_range('A5:C5', '')
-#chsc.merge_range('D2:F2', '')
-#chsc.merge_range('D3:F3', '')
-#chsc.merge_range('D4:F4', '')
-#chsc.merge_range('G1:R1', '')
-#chsc.merge_range('G2:I2', '')
-#chsc.merge_range('G3:I3', '')
-#chsc.merge_range('G4:I4', '')
-#chsc.merge_range('J2:J2', '')
-#chsc.merge_range('J3:L3', '')
-#chsc.merge_range('J4:L4', '')
-#chsc.merge_range('M2:O2', '')
-#chsc.merge_range('M3:O3', '')
-#chsc.merge_range('M4:I4', '')
-#chsc.merge_range('P2:R2', '')
-#chsc.merge_range('P3:R3', '')
-#chsc.merge_range('M4:I4', '')
+project = 'project#'
+chsc_worksheet.set_header(f'&LProject:\nother text')
+
+chsc_worksheet.set_column('A:C', 15, ID_format)
+chsc_worksheet.set_column('D:AA', 12, data_format)
+chsc_worksheet.set_column('AB:AG', 12, ID_format)
+
+#assay_startdate_col =chsc_read.loc[]
+#chsc_worksheet.write_datetime('AE', datetime.date('%d%b%y'), ID_format)
+
+chsc_worksheet.merge_range('D1:F1', '')
+chsc_worksheet.merge_range('F2:F3', '')
+#chsc_worksheet.merge_range('D3:F3', '')
+chsc_worksheet.merge_range('G1:I1', '')
+chsc_worksheet.merge_range('G2:I2', '')
+chsc_worksheet.merge_range('G3:I3', '')
+chsc_worksheet.merge_range('J1:L1', '')
+chsc_worksheet.merge_range('J2:L2', '')
+chsc_worksheet.merge_range('J3:L3', '')
+chsc_worksheet.merge_range('M1:O1', '')
+chsc_worksheet.merge_range('M2:O2', '')
+chsc_worksheet.merge_range('M3:O3', '')
+chsc_worksheet.merge_range('P1:R1', '')
+chsc_worksheet.merge_range('P2:R2', '')
+chsc_worksheet.merge_range('P3:R3', '')
+chsc_worksheet.merge_range('S1:U1', '')
+chsc_worksheet.merge_range('S2:U2', '')
+chsc_worksheet.merge_range('S3:U3', '')
+chsc_worksheet.merge_range('V1:X1', '')
+chsc_worksheet.merge_range('V2:X2', '')
+chsc_worksheet.merge_range('V3:X3', '')
+chsc_worksheet.merge_range('Y1:AA1', '')
+chsc_worksheet.merge_range('Y2:AA2', '')
+chsc_worksheet.merge_range('Y3:AA3', '')
 
 
-writer.save() 
+
+#for f in range(len(chsc_read.index)):
+#    i=0
+#    f=1
+#    if chsc_read.loc[3][f] != None:
+#        i+=f
+#        chsc_worksheet.merge_range(3,0,3,f,'')
+        
+
+
+
+
+chsc_writer.save() 
